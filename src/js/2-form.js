1c03da9bf6725a2a29582657704392ae97c310a1
Luidmyla Karmeliuk <lkarm67@gmail.com>
2025-07-06 10:51:34 +0300

    fix html,css,js

---
+++ src/js/2-form.js
@@ -0,0 +1,44 @@
+const formData = {
+    email: "",
+    message: ""
+  };
+  
+  const form = document.querySelector(".feedback-form");
+  const email = form.elements.email;
+  const textarea = form.elements.message;
+  const localStorageKey = "feedback-form-state";
+  
+  document.addEventListener("DOMContentLoaded", () => {
+    const savedFormData = localStorage.getItem(localStorageKey);
+    if (savedFormData) {
+      const parsedFormData = JSON.parse(savedFormData);
+      formData.email = parsedFormData.email || "";
+      formData.message = parsedFormData.message || "";
+  
+      email.value = formData.email;
+      textarea.value = formData.message;
+    }
+  });
+  
+  form.addEventListener("input", evt => {
+    const { name, value } = evt.target;
+    formData[name] = value.trim();
+    localStorage.setItem(localStorageKey, JSON.stringify(formData));
+  });
+  
+  form.addEventListener("submit", evt => {
+    evt.preventDefault();
+  
+    if (formData.email === "" || formData.message === "") {
+      alert("Fill please all fields");
+      return;
+    }
+  
+    console.log(formData);
+  
+    localStorage.removeItem(localStorageKey);
+    formData.email = "";
+    formData.message = "";
+    form.reset();
+  });
+  
