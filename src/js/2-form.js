f29641ee9286cf6441fefe941e875fa31e4ddbd9
Luidmyla Karmeliuk <lkarm67@gmail.com>
2025-07-06 22:33:42 +0300

    fix 2-form.js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,44 +1,36 @@
+const form = document.querySelector(".feedback-form");
+const email = form.elements.email;
+const textarea = form.elements.message;
+const localStorageKey = "feedback-form-state";
+
+const savedFormData = JSON.parse(localStorage.getItem(localStorageKey)) || {};
+
 const formData = {
-    email: "",
-    message: ""
-  };
-  
-  const form = document.querySelector(".feedback-form");
-  const email = form.elements.email;
-  const textarea = form.elements.message;
-  const localStorageKey = "feedback-form-state";
-  
-  document.addEventListener("DOMContentLoaded", () => {
-    const savedFormData = localStorage.getItem(localStorageKey);
-    if (savedFormData) {
-      const parsedFormData = JSON.parse(savedFormData);
-      formData.email = parsedFormData.email || "";
-      formData.message = parsedFormData.message || "";
-  
-      email.value = formData.email;
-      textarea.value = formData.message;
-    }
-  });
-  
-  form.addEventListener("input", evt => {
-    const { name, value } = evt.target;
-    formData[name] = value.trim();
-    localStorage.setItem(localStorageKey, JSON.stringify(formData));
-  });
-  
-  form.addEventListener("submit", evt => {
-    evt.preventDefault();
-  
-    if (formData.email === "" || formData.message === "") {
-      alert("Fill please all fields");
-      return;
-    }
-  
-    console.log(formData);
-  
-    localStorage.removeItem(localStorageKey);
-    formData.email = "";
-    formData.message = "";
-    form.reset();
-  });
-  
+  email: savedFormData.email || "",
+  message: savedFormData.message || ""
+};
+
+email.value = formData.email;
+textarea.value = formData.message;
+
+form.addEventListener("input", evt => {
+  const { name, value } = evt.target;
+  formData[name] = value.trim();
+  localStorage.setItem(localStorageKey, JSON.stringify(formData));
+});
+
+form.addEventListener("submit", evt => {
+  evt.preventDefault();
+
+  if (formData.email === "" || formData.message === "") {
+    alert("Fill please all fields");
+    return;
+  }
+
+  console.log(formData);
+
+  localStorage.removeItem(localStorageKey);
+  formData.email = "";
+  formData.message = "";
+  form.reset();
+});
